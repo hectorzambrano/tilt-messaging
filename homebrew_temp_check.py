f0922c3596d0a2201f431138fa12d25c1a4e2d48
Hector A Zambrano <65422369+hectorzambrano@users.noreply.github.com>
2021-05-25 14:31:35 -0500

    Update homebrew_temp_check.py
    Added scheduler to run data check every hour.

--- homebrew_temp_check.py
+++ homebrew_temp_check.py
@@ -23,6 +23,14 @@ import pickle
 import os.path
 
 import pandas as pd
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+sheet_name = input("Enter spreadsheet ID: ")
+low_alert = input("Enter low temperature alert: ")
+high_alert = input("Enter high temperature alert: ")
+personal_number = input("Enter personal phone number: ")
+twilio_number = input("Enter Twilio phone number: ")
 
 # Find your Account SID and Auth Token at twilio.com/console
 # See http://twil.io/secure
@@ -35,77 +43,78 @@ client = Client(account_sid, auth_token)
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# Function to obtain Google API credentials
-def gsheet_api_check(SCOPES):
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+def temp_check():   #function to run every 1 hour
+
+    # Function to obtain Google API credentials
+    def gsheet_api_check(SCOPES):
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES) # user needs to save json file in working directory after setting up credentials
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        return creds
+
+    # Function to call Google API and copy data from spreadsheet
+
+    from googleapiclient.discovery import build
+    def pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL):
+        creds = gsheet_api_check(SCOPES)
+        service = build('sheets', 'v4', credentials=creds)
+        sheet = service.spreadsheets()
+        result = sheet.values().get(
+            spreadsheetId=SPREADSHEET_ID,
+            range=DATA_TO_PULL).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES) # user needs to save json file in working directory after setting up credentials
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    return creds
+            rows = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                      range=DATA_TO_PULL).execute()
+            data = rows.get('values')
+            print("COMPLETE: Data copied")
+            return data
 
-# Function to call Google API and copy data from spreadsheet
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    DATA_TO_PULL = 'Data'
+    SPREADSHEET_ID = sheet_name
+    data = pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL)
 
-from googleapiclient.discovery import build
-def pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL):
-    creds = gsheet_api_check(SCOPES)
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-    result = sheet.values().get(
-        spreadsheetId=SPREADSHEET_ID,
-        range=DATA_TO_PULL).execute()
-    values = result.get('values', [])
+    column_names = ["Timestamp", "Timepoint", "SG", "Temperature", "Color", "Beer", "Comment"]
+    df = pd.DataFrame(data, columns=column_names)
+    df = df.drop(df.index[0])
+    df = df.set_index('Timestamp')
+    df = df.sort_values(by='Timestamp', ascending = False) # sorts by most recent Tilt Hydrometer reading
 
-    if not values:
-        print('No data found.')
-    else:
-        rows = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                  range=DATA_TO_PULL).execute()
-        data = rows.get('values')
-        print("COMPLETE: Data copied")
-        return data
+    # print(df)
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SPREADSHEET_ID = input("Enter spreadsheet ID: ")
-DATA_TO_PULL = 'Data'
-data = pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL)
+    current_temp = df.iloc[0,2]
+    # print(current_temp)
 
-column_names = ["Timestamp", "Timepoint", "SG", "Temperature", "Color", "Beer", "Comment"]
-df = pd.DataFrame(data, columns=column_names)
-df = df.drop(df.index[0])
-df = df.set_index('Timestamp')
-df = df.sort_values(by='Timestamp', ascending = False) # sorts by most recent Tilt Hydrometer reading
+    if current_temp <= low_alert:
+        message = client.messages \
+                    .create(
+                         body="Low fermentation temperature. Currently " + current_temp + " degF.",
+                         from_= twilio_number,
+                         to= personal_number
+                     )
 
-# print(df)
+    if current_temp >= high_alert:
+        message = client.messages \
+                    .create(
+                         body="High fermentation temperature. Currently " + current_temp + " degF.",
+                         from_= twilio_number,
+                         to= personal_number
+                     )
 
-current_temp = df.iloc[0,2]
-# print(current_temp)
-
-low_alert = input("Enter low temperature alert: ")
-high_alert = input("Enter high temperature alert: ")
-personal_number = input("Enter personal phone number: ")
-twilio_number = input("Enter Twilio phone number: ")
-
-if current_temp <= low_alert:
-    message = client.messages \
-                .create(
-                     body="Low fermentation temperature.",
-                     from_= twilio_number,
-                     to= personal_number
-                 )
-
-if current_temp >= high_alert:
-    message = client.messages \
-                .create(
-                     body="High fermentation temperature.",
-                     from_= twilio_number,
-                     to= personal_number
-                 )
+scheduler = BlockingScheduler()
+scheduler.add_job(temp_check, 'interval', hours=1)
+scheduler.start()
